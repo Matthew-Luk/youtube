bc323145eb9ac6ac1e648d9adeb807f3eab4d505
Matthew Luk <matthew.luk@yahoo.com>
2023-07-09 19:03:48 -0700

    added function to parse HTML entities

--- client/src/Components/functions.js
+++ client/src/Components/functions.js
@@ -76,3 +76,11 @@ export function convertDate2(publishedDate){
         }
     }
 }
+
+export function parseHtmlEntities(str) {
+    str = str.replace("&amp;", "&")
+    return str.replace(/&#([0-9]{1,3});/gi, function(match, numStr) {
+        var num = parseInt(numStr, 10)
+        return String.fromCharCode(num)
+    })
+}
